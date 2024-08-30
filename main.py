6fc34b7af89fd0f12d5ca7eea9579d3ddbc56886
Kareem <mikun4kareem@gmail.com>
2024-08-31 00:42:02 +0100

    first commit

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from time import sleep
-from simplegmail import Gmail
 import random
-import pandas
+import pandas as pd
 import datetime
 import os
-
-data = pandas.read_csv("startups.csv")
-names = data["Name"]
-companies = data["Company"]
-emails = data["Email"]
-
-gmail = Gmail()  # will open a browser window to ask you to log in and authenticate
+from time import sleep
+from simplegmail import Gmail
 
 
-# Function to keep track of the last email index sent and the date
+def initialize_gmail_objects(sender_creds):
+    gmail_objects = []
+    for creds in sender_creds:
+        gmail = Gmail(creds_file=creds["creds_file"])
+        gmail_objects.append({
+            "gmail": gmail,
+            "sender_email": creds["sender_email"]
+        })
+    return gmail_objects
+
+
 def get_last_email_index():
     if os.path.exists("last_email_index.txt"):
         with open("last_email_index.txt", "r") as file:
@@ -23,7 +26,7 @@ def get_last_email_index():
             if last_date == str(datetime.date.today()):
                 return last_index
             else:
-                return -1  # Reset index if the day has changed
+                return last_index  # Reset index if the day has changed
     return -1
 
 
@@ -32,7 +35,7 @@ def set_last_email_index(index):
         file.write(f"{str(datetime.date.today())},{index}")
 
 
-def email_script(name, company):
+def email_script(name, company, location):
     lis1_1 = ['hope', 'trust', 'wish']
     lis1_2 = ['finds', 'reaches', 'meets']
     lis1_3 = ['excited', 'thrilled', 'pleased']
@@ -43,49 +46,54 @@ def email_script(name, company):
     lis1_8 = ['add value', 'contribute', 'be an asset']
     lis1_9 = ['considering', 'reviewing', 'taking the time to consider']
     lis1_0 = ['Hey', 'Dear', 'Hi']
-    script = f"""
-        <p>{random.choice(lis1_0)} {name.capitalize()},</p>
 
-        <p>
-            I {random.choice(lis1_1)} this email {random.choice(lis1_2)} you well. I recently learned about your {company.capitalize()}'s successful funding round and was {random.choice(lis1_3)} to hear about your growth plans. I am writing to express my interest in joining your team as a Software Engineer.
-        </p>
-
-        <p>
-            Based in Nigeria, I offer a {random.choice(lis1_4)} that is both cost-effective and efficient. With my experience in {random.choice(lis1_5)}, mobile development, and API integrations, combined with my ability to {random.choice(lis1_6)} new tech stacks, I am confident in my ability to contribute to your projects and help {random.choice(lis1_7)} your success.
-        </p>
-
-        <p>
-            Please find my CV attached for your review. I would love the opportunity to discuss how I can {random.choice(lis1_8)} to your team.
-        </p>
-
-        <p>
-            Thank you for {random.choice(lis1_9)} my application.
-        </p>
-
-        <p>
-            Best regards,<br>
-            Elelu Abdulkareem Ayomikun<br>
-            <a href="https://www.linkedin.com/in/abdulkareem-elelu-8174b1237/" target="_blank">LinkedIn Profile</a> | +2349023058977
-        </p>
-    """
+    script = f"""<p>{random.choice(lis1_0)} {name.capitalize()},</p>
+        <p>I {random.choice(lis1_1)} this email {random.choice(lis1_2)} you well. I recently learned about your {company.capitalize()}'s successful funding round and was {random.choice(lis1_3)} to hear about your growth plans. I am writing to express my interest in joining your team as a Software Engineer.</p>
+        <p>Based in Nigeria, I offer a remote work solution that would cost less than 50% of what you'd typically pay for a software engineer in {location}. With my extensive experience in full stack development, {random.choice(lis1_5)}, mobile app development, and API integrations, combined with my ability to {random.choice(lis1_6)} new tech stacks, I am confident that I can deliver high-quality work at a fraction of the usual cost.</p>
+        <p>Please find my CV attached for your {random.choice(["review", "consideration"])}. I would love the opportunity to discuss how I can {random.choice(lis1_8)} to your team, even if you're not actively hiring.</p>
+        <p>Thank you for {random.choice(lis1_9)} my application.</p>
+        <p>Best regards,<br>Elelu Abdulkareem Ayomikun<br><a href="https://www.linkedin.com/in/abdulkareem-elelu-8174b1237/" target="_blank">LinkedIn Profile</a> | +2349023058977</p>"""
     return script
 
 
-# Get the last email index sent today
-start_index = get_last_email_index() + 1
+class EmailSender:
+    def __init__(self, data_file, sender_creds):
+        self.data = pd.read_csv(data_file)
+        self.names = self.data["Name"]
+        self.companies = self.data["Company"]
+        self.emails = self.data["Email"]
+        self.locations = self.data["HQ"]
+        self.gmail_objects = initialize_gmail_objects(sender_creds)
 
-# Send 10 emails
-for i in range(start_index, start_index + 10):
-    if i < len(companies):
-        params = {
-            "to": f"{emails[i]}",
-            "sender": "Abdulkareem <eleluabdulkareem@gmail.com>",
-            "subject": f"Remote Software Engineer at {companies[i]}",
-            "msg_html": email_script(names[i], companies[i]),
-            "attachments": ["resume.pdf"],
-            "signature": True  # use my account signature
-        }
-        message = gmail.send_message(**params)
-        print(f"Application successfully sent to {companies[i]}")
-        set_last_email_index(i)
-        sleep(300)  # Sleep for 5 minutes between sending each email
+    def send_emails(self, start_index, num_emails=10):
+        for i in range(start_index, start_index + num_emails):
+            if i < len(self.companies):
+                gmail_obj = random.choice(self.gmail_objects)
+                gmail = gmail_obj["gmail"]
+                sender_email = gmail_obj["sender_email"]
+
+                params = {
+                    "to": self.emails[i],
+                    "sender": sender_email,
+                    "subject": f"Remote Software Engineer at {self.companies[i]}",
+                    "msg_html": email_script(self.names[i], self.companies[i], self.locations[i]),
+                    "attachments": ["resume.pdf"],
+                    "signature": True  # use account signature
+                }
+                message = gmail.send_message(**params)
+                print(f"Application successfully sent to {self.companies[i]} from {sender_email}")
+                set_last_email_index(i)
+                sleep(300)  # Sleep for 5 minutes between sending each email
+
+
+if __name__ == "__main__":
+    sender_creds = [
+        {"creds_file": "gmail_token(justkareem).json", "sender_email": "Abdulkareem <justkareemelelu@gmail.com>"},
+        {"creds_file": "gmail_token_mss.json", "sender_email": "Abdulkareem <mss.abdulkareem.elelu@tau.edu.ng>"},
+        {"creds_file": "gmail_token(eleluabdulkareem).json", "sender_email": "Abdulkareem Elelu <eleluabdulkareem@gmail.com>"},
+        # Add more sender credentials and emails if needed
+    ]
+
+    email_sender = EmailSender("startups (1).csv", sender_creds)
+    start_index = get_last_email_index() + 1
+    email_sender.send_emails(start_index, 10)
